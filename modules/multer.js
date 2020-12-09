99c45e33403b1a5c423b1b33dc0af62bad1d03fc
oh980225 <oh980225@gmail.com>
2020-12-09 23:30:10 +0900

    Add 'Read All Banner Images'

--- modules/multer.js
+++ modules/multer.js
@@ -7,7 +7,7 @@ const s3 = new aws.S3();
 const upload = multer({
     storage: multerS3({
         s3,
-        bucket: 'sopt-27-epitone',
+        bucket: 'sopt-27-osj', // bucket으로 변경 sopt-27-epitone -> sopt-27-osj
         acl: 'public-read',
         key: function(req, file, cb){
             cb(null, 'images/origin/' + Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
